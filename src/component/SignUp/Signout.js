8e2e04de052db59a266e4592430108264a8947d3
CHO SUAH <102336763+Suah-Cho@users.noreply.github.com>
2023-11-24 15:19:53 +0900

    Update Signout.js

--- src/component/SignUp/Signout.js
+++ src/component/SignUp/Signout.js
@@ -4,13 +4,13 @@ const SignOut = () => {
 
     const onSignout = () => {
         alert('회원 탈퇴되었습니다:)')
-        axios.delete(`${process.env.REACT_APP_EC2_API_URL}/signout/${sessionStorage.getItem('token')}`)
+        axios.delete(`${process.env.REACT_APP_EC2_API_URL}/signout/${sessionStorage.getItem('userId')}`)
         .then(response => {
             console.log(response.data);
         }).catch(error => {
             console.log(error);
         })
-        sessionStorage.removeItem('token');
+        sessionStorage.removeItem('userId');
         document.location.href = '/'
     }
     return (
